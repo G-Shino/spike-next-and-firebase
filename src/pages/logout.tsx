b005a670df695fd26a132c4709f005b491ea9418
G-shino <shinogu2929@gmail.com>
2020-05-25 01:49:30 +0900

    add simple message list

--- src/pages/logout.tsx
+++ src/pages/logout.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Router from "next/router";
 import firebase from "./../lib/firebase";
 import { FontSize } from "./../constants/Font";
@@ -7,6 +7,7 @@ import { TextField, Button } from "@material-ui/core";
 
 const Logout: React.FC = () => {
   const [message, setMessage] = useState("");
+  const [messageList, setMessageList] = useState<string[]>([]);
 
   const handleLogoutButtonClick = async (
     e: React.MouseEvent<HTMLButtonElement>
@@ -23,6 +24,39 @@ const Logout: React.FC = () => {
   const handleMessageTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setMessage(e.target.value);
   };
+
+  const handleSendButtonClick = async (
+    e: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    console.log(e);
+    try {
+      firebase.database().ref("message").push({
+        message,
+        createdAt: firebase.database.ServerValue.TIMESTAMP,
+      });
+      console.log("message send: OK");
+    } catch (error) {
+      console.error("message send: ERROR", error);
+    }
+  };
+
+  useEffect(() => {
+    const messageRef = firebase
+      .database()
+      .ref("message")
+      .orderByChild("createdAt");
+    messageRef.off("child_added");
+    messageRef.on("child_added", (messageSnapshot) => {
+      const messageId = messageSnapshot.key;
+      const messageData = messageSnapshot.val();
+      console.log(messageId, messageData);
+      setMessageList((prev) => [...prev, messageData.message]);
+    });
+    return () => {
+      messageRef.off("child_added");
+    };
+  }, []);
+
   return (
     <WrapperDiv>
       <TitleH1>Send Your Message!!</TitleH1>
@@ -42,8 +76,18 @@ const Logout: React.FC = () => {
           value={message}
           onChange={handleMessageTextChange}
         ></StyledTextField>
-        <StyledSendButton variant="outlined">Send</StyledSendButton>
+        <StyledSendButton
+          variant="outlined"
+          type="button"
+          disabled={message === ""}
+          onClick={handleSendButtonClick}
+        >
+          Send
+        </StyledSendButton>
       </StyledMessageForm>
+      {messageList.map((message, idx) => (
+        <p key={idx}>{message}</p>
+      ))}
     </WrapperDiv>
   );
 };
